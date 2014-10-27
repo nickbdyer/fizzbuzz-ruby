3f6ab6621bb680450ad2749f98787c545dd3c82a
Nick Dyer <nbdyer@gmail.com>
2014-10-27 16:25:39 +0000

    First commit

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,30 @@
+require 'fizzbuzz'
+
+  describe 'fizzbuzz' do
+    
+    context 'knows when a number' do
+
+      it 'is divisible by three' do
+        expect(is_divisible_by_three?(3)).to be true
+      end
+
+      it 'is divisible by five' do 
+        expect(is_divisible_by_five?(5)).to be true
+      end
+
+      it 'is NOT divisible by three' do
+        expect(is_divisible_by_three?(1)).to be false
+      end
+
+      it 'is NOT divisble by five' do
+        expect(is_divisible_by_five?(1)).to be false
+      end
+
+      it 'is divisible by fifteen' do
+        expect(is_divisible_by_fifteen?(15)).to be true
+      end
+
+
+    end
+
+  end
