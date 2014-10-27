9bc9a3b5f8943c6398721cda3c20c8bcc6dd3f32
Nick Dyer <nbdyer@gmail.com>
2014-10-27 16:38:43 +0000

    refactor is divisble by

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,16 +12,16 @@ require 'fizzbuzz'
         expect(is_divisible_by_five?(5)).to be true
       end
 
+      it 'is divisible by fifteen' do
+        expect(is_divisible_by_fifteen?(15)).to be true
+      end
+
       it 'is NOT divisible by three' do
         expect(is_divisible_by_three?(1)).to be false
       end
 
       it 'is NOT divisble by five' do
         expect(is_divisible_by_five?(1)).to be false
-      end
-
-      it 'is divisible by fifteen' do
-        expect(is_divisible_by_fifteen?(15)).to be true
       end
 
       it 'is NOT divisible by fifteen' do
